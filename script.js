86767baf376c482d9b9e7cce9964f4a7d7d0761b
appidaniel97 <appidaniel97@gmail.com>
2021-10-27 00:21:46 -0300

    estrutura inicial

---
+++ script.js
@@ -0,0 +1,23 @@
+var hamburguer = document.querySelector(".Menuhamburger");
+
+hamburguer.addEventListener("click", function()
+{
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+
+
+
+window.onscroll=function(){
+    fixarMenuNoTopo();
+}
+const menu=document.getElementsByTagName("menu")[0];
+const topoNav=menu.offsetTop;
+
+function fixarMenuNoTopo(){
+	if(window.pageYOffset >= topoNav){
+	nav.classList.add("FixoNoTopo");
+	}else{
+	        nav.classList.remove("FixoNoTopo");
+    }
+
+}           
